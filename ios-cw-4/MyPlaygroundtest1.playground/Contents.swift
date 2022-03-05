ffb6e1445b11da63ff61375782e321f86389a3e6
aishaALKM <70426536+aishaALKM@users.noreply.github.com>
2022-03-05 13:38:51 +0300

    doneeeee

---
+++ ios-cw-4/MyPlaygroundtest1.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var greeting = "السلام عليكم،معاكم"
+
+let my_name = "عايشة"
+
+var age = 16
+
+var hight = 15.9
+
+let yearOfBrith = 2005
+
+var favoriteNumber = 3
+
+var hobby = "coding"
+
+print("السلام عليكم معاكم", my_name," ورقمي المفضل هو", favoriteNumber ,"و احب ال ",hobby)
+
